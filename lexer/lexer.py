879fff5b30481257a79df9e1818d37a8097e5f93
Luis Macedo <luismacedo17@gmail.com>
2026-05-05 20:27:51 +0100

    feat: enhance lexer with keyword support and token definitions

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,5 +1,18 @@
 import ply.lex as lex
 
+# Keywords
+reserved = {
+    'let': 'LET',
+    'fun': 'FUN',
+    'if': 'IF',
+    'then': 'THEN',
+    'else': 'ELSE',
+    'Int': 'INT_TYPE',
+    'Bool': 'BOOL_TYPE',
+    'true': 'TRUE',
+    'false': 'FALSE',
+}
+
 # Tokens
 tokens = (
     'NUMBER',
@@ -10,7 +23,10 @@ tokens = (
     'LPAREN',
     'RPAREN',
     'SEMI',
-)
+    'COLON',
+    'EQUALS',
+    'ID',
+) + tuple(reserved.values())
 
 # Regras simples
 t_PLUS   = r'\+'
@@ -20,17 +36,30 @@ t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_SEMI   = r';'
+t_COLON  = r':'
+t_EQUALS = r'='
 
-# Número inteiro
+# Número
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
+# Identificadores + keywords
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')  # 👈 chave aqui
+    return t
+
 # Ignorar espaços
 t_ignore = ' \t'
 
-# Contar linhas
+# Comentários
+def t_COMMENT(t):
+    r'--.*'
+    pass
+
+# Linhas
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
@@ -40,5 +69,5 @@ def t_error(t):
     print(f"Illegal character: {t.value[0]}")
     t.lexer.skip(1)
 
-# Criar lexer
+# Build
 lexer = lex.lex()
